18d3fe75eaa5be38db215b224f0036988962ecb0
Goga <googlichek.one@gmail.com>
2018-05-25 13:54:14 +0300

    And now Bishop & Queen.

--- Assets/_Project/Scripts/Pieces/Bishop.cs
+++ Assets/_Project/Scripts/Pieces/Bishop.cs
@@ -2,8 +2,49 @@
 {
 	public class Bishop : BasePiece
 	{
+		private const int SearchLength = GlobalVariables.BoardLength - 1;
+
 		public override void FindCellsToHighlight()
 		{
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column + index * DistanceUnit,
+						Row + index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column + index * DistanceUnit,
+						Row - index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column - index * DistanceUnit,
+						Row + index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column - index * DistanceUnit,
+						Row - index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
 		}
 	}
 }
--- Assets/_Project/Scripts/Pieces/Queen.cs
+++ Assets/_Project/Scripts/Pieces/Queen.cs
@@ -2,8 +2,77 @@
 {
 	public class Queen : BasePiece
 	{
+		private const int SearchLength = GlobalVariables.BoardLength - 1;
+
 		public override void FindCellsToHighlight()
 		{
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column + index * DistanceUnit,
+						Row + index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column + index * DistanceUnit,
+						Row - index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column - index * DistanceUnit,
+						Row + index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(
+						Column - index * DistanceUnit,
+						Row - index * DistanceUnit,
+						Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(Column + index * DistanceUnit, Row, Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(Column - index * DistanceUnit, Row, Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(Column, Row + index * DistanceUnit, Side);
+				if (!isPositionClear) break;
+			}
+
+			for (int index = 1; index <= SearchLength; index++)
+			{
+				var isPositionClear =
+					FindPositionWithValidation(Column, Row - index * DistanceUnit, Side);
+				if (!isPositionClear) break;
+			}
 		}
 	}
 }
